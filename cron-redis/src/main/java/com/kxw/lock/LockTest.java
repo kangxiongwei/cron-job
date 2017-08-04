2b817e8ca7f3f94d7dc0284be8e3148538e4cbd5
kangxiongwei <kangxiongwei@meituan.com>
2017-08-04 11:55:26 +0800

    redis的分布式锁

---
+++ cron-redis/src/main/java/com/kxw/lock/LockTest.java
@@ -0,0 +1,32 @@
+package com.kxw.lock;
+
+import org.redisson.Redisson;
+import org.redisson.api.RExecutorService;
+import org.redisson.api.RLock;
+import org.redisson.api.RMap;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+
+/**
+ * Created by kangxiongwei on 2017/8/4 11:52.
+ */
+public class LockTest {
+
+    public static void main(String[] args) {
+        // 1. Create config object
+        Config config = new Config();
+
+        // 2. Create Redisson instance
+        RedissonClient redisson = Redisson.create(config);
+
+        // 3. Get object you need
+        RMap<String, String> map = redisson.getMap("myMap");
+
+        RLock lock = redisson.getLock("myLock");
+
+        RExecutorService executor = redisson.getExecutorService("myExecutorService");
+
+        // over 30 different objects and services ...
+    }
+
+}
